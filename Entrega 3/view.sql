8c1470bcce1e352594c2f4c62dc1b196f57ae545
Literalmentnerd <bernas.augusto@gmail.com>
2023-06-09 19:25:53 +0100

    view fixed city left

--- Entrega 3/view.sql
+++ Entrega 3/view.sql
@@ -1,6 +1,6 @@
 CREATE VIEW product_sales(SKU, order_no, qty, total_price, year, month, day_of_month, day_of_week, city)
 AS
-SELECT a.SKU, a.order_no, a.qty, (a.qty*e.price), EXTRACT(YEAR FROM b.date), EXTRACT(MONTH FROM b.date), EXTRACT(DAY FROM b.date), EXTRACT(DOW FROM b.date), city
+SELECT a.SKU, a.order_no, a.qty, (a.qty*d.price) AS total_price, EXTRACT(YEAR FROM b.date) AS year, EXTRACT(MONTH FROM b.date) AS month, EXTRACT(DAY FROM b.date) AS day, EXTRACT(DOW FROM b.date) AS day_of_the_week
 FROM contains a
 JOIN orders b ON a.order_no = b.order_no
 JOIN pay c ON a.order_no = c.order_no
