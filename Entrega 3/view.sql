5d12c3df09e99554235ea9a7cd98d438f82c16c8
Axiself <36574039+Axiself@users.noreply.github.com>
2023-06-09 21:25:20 +0100

    quick fix to view

--- Entrega 3/view.sql
+++ Entrega 3/view.sql
@@ -2,14 +2,14 @@ CREATE VIEW product_sales(SKU, order_no, qty, total_price, year, month, day_of_m
 AS
 SELECT  a.SKU,
         a.order_no, a.qty,
-        (a.qty*d.price) AS total_price,
-        EXTRACT(YEAR FROM c.date) AS year,
-        EXTRACT(MONTH FROM c.date) AS month,
-        EXTRACT(DAY FROM c.date) AS day,
-        EXTRACT(DOW FROM c.date) AS day_of_the_week,
-        SUBSTRING(e.address FROM '(?!,)[^,]+$') AS city
+        (a.qty*d.price),
+        EXTRACT(YEAR FROM c.date),
+        EXTRACT(MONTH FROM c.date),
+        EXTRACT(DAY FROM c.date),
+        EXTRACT(DOW FROM c.date),
+        SUBSTRING(e.address FROM '(?!,)[^,]+$')
 FROM contains a
 JOIN pay b ON a.order_no = b.order_no
 JOIN orders c ON a.order_no = c.order_no
 JOIN product d ON a.SKU = d.SKU
-JOIN customer e ON c.cust_no = e.cust_no
+JOIN customer e ON c.cust_no = e.cust_no;
