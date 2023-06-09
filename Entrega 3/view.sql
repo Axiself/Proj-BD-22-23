a98e4671030bc79a17fe08308159f08ea6627347
Literalmentnerd <bernas.augusto@gmail.com>
2023-06-09 19:32:09 +0100

    eat

--- Entrega 3/view.sql
+++ Entrega 3/view.sql
@@ -1,8 +1,8 @@
 CREATE VIEW product_sales(SKU, order_no, qty, total_price, year, month, day_of_month, day_of_week, city)
 AS
-SELECT a.SKU, a.order_no, a.qty, (a.qty*d.price) AS total_price, EXTRACT(YEAR FROM b.date) AS year, EXTRACT(MONTH FROM b.date) AS month, EXTRACT(DAY FROM b.date) AS day, EXTRACT(DOW FROM b.date) AS day_of_the_week
+SELECT a.SKU, a.order_no, a.qty, (a.qty*d.price) AS total_price, EXTRACT(YEAR FROM c.date) AS year, EXTRACT(MONTH FROM c.date) AS month, EXTRACT(DAY FROM c.date) AS day, EXTRACT(DOW FROM c.date) AS day_of_the_week
 FROM contains a
-JOIN orders b ON a.order_no = b.order_no
-JOIN pay c ON a.order_no = c.order_no
+JOIN pay b ON a.order_no = b.order_no
+JOIN orders c ON a.order_no = c.order_no
 JOIN product d ON a.SKU = d.SKU
-JOIN customer e ON b.cust_no = e.cust_no
+JOIN customer e ON c.cust_no = e.cust_no
