e8be3df68364c861c3e5b42f5d42264e582196ba
Literalmentnerd <bernas.augusto@gmail.com>
2023-06-11 21:07:16 +0100

    lets have this here for now

---
+++ Entrega 3/login.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+IST_ID = 'ist1102081'
+host = 'db.tecnico.ulisboa.pt'
+port = 5432
+password = 'TopSecret'
+db_name = IST_ID
+credentials = 'host={} port={} user={} password={} dbname={}'.format(host, port, IST_ID,
+password, db_name)
