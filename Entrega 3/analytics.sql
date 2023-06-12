355385f00d4c80a42797366acb919161ccdd683f
Literalmentnerd <bernas.augusto@gmail.com>
2023-06-12 15:18:12 +0100

    rollup

--- Entrega 3/analytics.sql
+++ Entrega 3/analytics.sql
@@ -3,7 +3,7 @@
 SELECT sku AS product, city, month, day_of_month, day_of_week, SUM(qty) AS total_amount, SUM(total_price) AS total_price
 FROM product_sales
 WHERE year = 2022
-GROUP BY CUBE(sku,city,month,day_of_the_month,day_of_the_week);
+GROUP BY ROLLUP(sku,city,month,day_of_the_month,day_of_the_week);
 
 --OLAP 2
 
