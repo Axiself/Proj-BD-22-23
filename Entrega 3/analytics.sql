11d845df7ca7a45c8f7301acc438c62ea0e79914
Literalmentnerd <bernas.augusto@gmail.com>
2023-06-12 15:29:36 +0100

    OLAP feito

--- Entrega 3/analytics.sql
+++ Entrega 3/analytics.sql
@@ -3,11 +3,11 @@
 SELECT sku AS product, city, month, day_of_month, day_of_week, SUM(qty) AS total_amount, SUM(total_price) AS total_price
 FROM product_sales
 WHERE year = 2022
-GROUP BY ROLLUP(sku,city,month,day_of_the_month,day_of_the_week);
+GROUP BY ROLLUP(sku,city,month,day_of_month,day_of_week);
 
 --OLAP 2
 
-SELECT month,day_of_the_week,AVG(total_price) AS valor_medio_diario
+SELECT month,day_of_week,AVG(total_price) AS valor_medio_diario
 FROM product_sales
 WHERE year = 2022
-GROUP BY CUBE(month,day_of_the_week,valor_medio_diario);
+GROUP BY CUBE(month,day_of_week,total_price);
